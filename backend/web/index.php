991e78ffbc05334f6de83a2ef30b901ba50119d7
Shekhovtsov Yuri <shekhovtsovy@yandex.ru>
2015-11-27 13:25:12 +0300

    structure clear

--- backend/web/index.php
+++ backend/web/index.php
@@ -9,9 +9,7 @@ require(__DIR__ . '/../config/bootstrap.php');
 
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../common/config/main.php'),
-    require(__DIR__ . '/../../common/config/main-local.php'),
-    require(__DIR__ . '/../config/main.php'),
-    require(__DIR__ . '/../config/main-local.php')
+    require(__DIR__ . '/../config/main.php')
 );
 
 $application = new yii\web\Application($config);
