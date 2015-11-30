aca9f6cbcce9cbb12245496159f42507b146c259
lowBase <lowbase@yandex.ru>
2015-11-30 12:11:22 +0300

    Update main.php

--- common/config/main.php
+++ common/config/main.php
@@ -35,10 +35,6 @@ return [
 
             ],
         ],
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
-        ],
         'i18n' => [
             'translations' => [
                 'eauth' => [
